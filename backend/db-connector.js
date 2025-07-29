a131ae68a67fa4f39e7823c8d6bd1054e8c767f7
Andy Li <54951411+SpicyRicecaker@users.noreply.github.com>
2025-07-29 13:55:50 -0700

    create example table

--- backend/db-connector.js
+++ backend/db-connector.js
@@ -2,13 +2,22 @@
 const mysql = require("mysql2");
 
 // Create a 'connection pool' using the provided credentials
+// const pool = mysql.createPool({
+//     waitForConnections: true,
+//     connectionLimit   : 10,
+//     host              : '***REDACTED***',
+//     user              : '***REDACTED***',
+//     password          : '***REDACTED***',
+//     database          : '***REDACTED***'
+// }).promise(); // This makes it so we can use async / await rather than callbacks
+
 const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
-    host              : '***REDACTED***',
-    user              : '***REDACTED***',
-    password          : '***REDACTED***',
-    database          : '***REDACTED***'
+    host              : 'localhost',
+    user              : 'root',
+    password          : 'asdf',
+    database          : 'test'
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
 // Export it for use in our application
