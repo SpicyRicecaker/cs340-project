c62da57206f021ce4d986adc9217b2f383b6f4ed
Andy Li <54951411+SpicyRicecaker@users.noreply.github.com>
2025-07-31 17:00:15 -0700

    create style changes

--- frontend/src/Link.jsx
+++ frontend/src/Link.jsx
@@ -1,6 +1,6 @@
 function Link( {url, label, inGrid} ) {
 
-    return <a href={url} className="@container p-3 rounded-lg border-2 border-solid text-center font-mono grid hover:(cursor-pointer) shadow-md">
+    return <a draggable={false} href={url} className="@container p-3 rounded-lg border-2 border-solid text-center font-mono grid hover:(cursor-pointer) shadow-md">
             <div className={`place-self-center ${inGrid ? 'text-[min(15cqh,15cqw)]' : ''}`}>{label}</div>
         </a>
 }
